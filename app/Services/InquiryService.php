a4c4e4c4d0d7de0eb2dda4f545810aba6b3311fa
h-fujita-nextat <h.fujita@nextat.co.jp>
2022-12-26 14:01:59 +0900

    メール送信時のエラー

---
+++ app/Services/InquiryService.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Http\Controllers\InquiryController;
+use App\Models\Inquiry;
+use App\Models\User;
+use App\Notifications\InquiryNotification;
+
+class InquiryService
+{
+    /**
+     * メールを送る
+     *
+     * @param User $user
+     * @param Inquiry $inquiry
+     */
+    public function send(User $user, Inquiry $inquiry): void
+    {
+        $user->notify(new InquiryNotification(
+            $inquiry
+        ));
+    }
+}
