a516fa49db28d1ac2b0a87a924721585c3aad4b4
h-fujita-nextat <h.fujita@nextat.co.jp>
2023-01-04 11:17:12 +0900

    修正完了

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,8 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
-use App\Events\InquiryCreated;
 
 class EventServiceProvider extends ServiceProvider
 {
